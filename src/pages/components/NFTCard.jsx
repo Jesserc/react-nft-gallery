7405eb0ddce838782b0ab3e2dd926fe3a3f35815
Jesserc <nodejesse@gmail.com>
2022-06-12 02:26:39 +0100

    updated UI for the NFT card component @ 2:26 am GMT+1

--- src/pages/components/NFTCard.jsx
+++ src/pages/components/NFTCard.jsx
@@ -6,8 +6,8 @@ export const NFTCard = ({nft}) => {
 			<div className="nft-image">
 				<img src={nft.media[0].gateway}></img>
 			</div>
-			<div>
-				<div className="">
+			<div className="nft-body">
+				<div className="nft-title">
 					<h2>{nft.title}</h2>
 					<p>Id: {nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
 					<p>{`${nft.contract.address.substr(
@@ -18,10 +18,10 @@ export const NFTCard = ({nft}) => {
 					)}`}</p>
 				</div>
 
-				<div>
+				<div className="nft-description">
 					<p>{nft.description?.substr(0, 150)}</p>
 				</div>
-				<div>
+				<div className="etherscan-btn">
 					<a
 						target={"_blank"}
 						href={`https://etherscan.io/token/${nft.contract.address}`}
